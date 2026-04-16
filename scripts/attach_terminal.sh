579c24288707b949af71c5f7cc0e1fc5c752f48c
shawky-dev <shawky.projects25@gmail.com>
2026-04-16 22:44:17 +0200

    fixed attack_terminal.sh not finding docker id

--- scripts/attach_terminal.sh
+++ scripts/attach_terminal.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Get the container ID
-CONTAINER=$(docker container ls | grep -E "ros2_devcontainer" | awk '{print $1}')
+# Get the container ID by matching the image name prefix
+CONTAINER=$(docker container ls --format '{{.ID}} {{.Image}} {{.Names}}' | grep -i "ros2-devcontainer" | awk '{print $1}')
 
 # Execute the entrypoint script inside the container
 docker container exec -it "$CONTAINER"  /entrypoint.sh tmux
